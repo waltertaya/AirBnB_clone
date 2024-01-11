60970027f4744f975b07796c9c4c0e96986ff956
waltertaya <waltermittybecker5@gmail.com>
2024-01-11 15:53:46 +0300

    Update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,16 @@ import uuid
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())  # unique id
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) != 0:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         print("[{}] ({}) {}".format(self.__class__.__name__, self.id,
