14d45fe1a3ce38839c801698ff8f6a2d6db2451a
waltertaya <waltermittybecker5@gmail.com>
2024-01-14 23:37:20 +0300

    Updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""Unittest class for testing BaseModel class """
 from models.base_model import BaseModel
 import json
 import os
@@ -34,19 +34,19 @@ class TestBaseModel(unittest.TestCase):
             pass
 
     def test_default(self):
-        """ """
+        """Test default """
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """Test kwargs """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """test_kwargs_int """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -54,25 +54,25 @@ class TestBaseModel(unittest.TestCase):
             new = BaseModel(**copy)
 
     def test_kwargs_one(self):
-        """ """
+        """Test kwargs_one """
         m = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**m)
 
     def test_kwargs_none(self):
-        """ """
+        """Test kwargs_none """
         m = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**m)
 
     def test_str(self):
-        """ """
+        """Test __str__ method"""
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ """
+        """Test to_dict() method"""
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
@@ -87,12 +87,12 @@ class TestBaseModel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_id(self):
-        """ """
+        """Test id attribute """
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_updated_at(self):
-        """ """
+        """Test updated_date attribute """
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
@@ -100,6 +100,6 @@ class TestBaseModel(unittest.TestCase):
         self.assertFalse(new.created_at == new.updated_at)
 
     def test_created_at(self):
-        """ """
+        """Test created_at attribute """
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
