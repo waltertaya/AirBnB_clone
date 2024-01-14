850c0c88f316000ded0bbd9befd218af1c72ddc9
waltertaya <waltermittybecker5@gmail.com>
2024-01-14 14:42:08 +0300

    Create tests/test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,47 +1,41 @@
-#!/usr/bin/env python3
-"""Unittest for command line interpreter"""
+#!/usr/bin/python3
+"""
+Contains the class TestConsoleDocs
+"""
 
-import unittest
-import pep8
 import console
 import inspect
+import pep8
+import unittest
 HBNBCommand = console.HBNBCommand
 
 
-class TestConsole(unittest.TestCase):
-    """Unittest for command line interpreter"""
+class TestConsoleDocs(unittest.TestCase):
+    """Class for testing documentation of the console"""
+    def test_pep8_conformance_console(self):
+        """Test that console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_pep8_console(self):
-        """Tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        result = style.check_files(['console.py'])
-        self.assertEqual(result.total_errors, 0, "fix pep8")
-
-    def test_docstrings_in_console(self):
-        """Tests docstrings"""
-        self.assertIsNot(console.__doc__, None,
-                         "Missing docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
-                        "Missing docstring")
-
-    def test_docstrings_in_class(self):
-        """Tests docstrings"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
-                         "Missing docstring")
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
-                        "Missing docstring")
+    def test_pep8_conformance_test_console(self):
+        """Test that tests/test_console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_console_module_docstring(self):
-        """Test console.py module docstring"""
+        """Test for the console.py module docstring"""
         self.assertIsNot(console.__doc__, None,
                          "console.py needs a docstring")
         self.assertTrue(len(console.__doc__) >= 1,
                         "console.py needs a docstring")
 
-    def test_console_function_docstrings(self):
-        """Test docstrings in console.py functions"""
-        for func in dir(HBNBCommand):
-            self.assertIsNot(func.__doc__, None,
-                             "{:s} method needs a docstring".format(func))
-            self.assertTrue(len(func.__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func))
+    def test_HBNBCommand_class_docstring(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
