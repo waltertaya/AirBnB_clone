7b26e46c0088dbb35293a52b859f5748b0482e82
waltertaya <waltermittybecker5@gmail.com>
2024-01-14 15:18:25 +0300

    Fix base_model

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ from models import storage
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-
+        """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
